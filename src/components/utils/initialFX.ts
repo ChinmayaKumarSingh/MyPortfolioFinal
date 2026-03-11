f3a60a34338495034874cde79464e265fc12a7b9
ChinmayaSingh <chinmaya.technology@gmail.com>
2026-03-12 00:42:17 +0530

    remove gsap trial version

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -9,12 +9,11 @@ gsap.registerPlugin(SplitText);
 
 export function initialFX() {
   document.body.style.overflowY = "auto";
+
   smoother.paused(false);
 
   const main = document.getElementsByTagName("main")[0];
-  if (main) {
-    main.classList.add("main-active");
-  }
+  if (main) main.classList.add("main-active");
 
   gsap.to("body", {
     backgroundColor: "#0a0e17",
@@ -61,90 +60,4 @@ export function initialFX() {
       delay: 0.3,
     }
   );
-
-  gsap.fromTo(
-    ".landing-info-h2",
-    { opacity: 0, y: 30 },
-    {
-      opacity: 1,
-      duration: 1.2,
-      ease: "power1.inOut",
-      y: 0,
-      delay: 0.8,
-    }
-  );
-
-  gsap.fromTo(
-    [".header", ".icons-section", ".nav-fade"],
-    { opacity: 0 },
-    {
-      opacity: 1,
-      duration: 1.2,
-      ease: "power1.inOut",
-      delay: 0.1,
-    }
-  );
-
-  const landingText3 = new SplitText(".landing-h2-info-1", TextProps);
-  const landingText4 = new SplitText(".landing-h2-1", TextProps);
-  const landingText5 = new SplitText(".landing-h2-2", TextProps);
-
-  LoopText(landingText2, landingText3);
-  LoopText(landingText4, landingText5);
-}
-
-function LoopText(Text1: any, Text2: any) {
-  const tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
-
-  const delay = 4;
-  const delay2 = delay * 2 + 1;
-
-  tl.fromTo(
-    Text2.chars,
-    { opacity: 0, y: 80 },
-    {
-      opacity: 1,
-      duration: 1.2,
-      ease: "power3.inOut",
-      y: 0,
-      stagger: 0.1,
-      delay: delay,
-    },
-    0
-  )
-    .fromTo(
-      Text1.chars,
-      { y: 80 },
-      {
-        duration: 1.2,
-        ease: "power3.inOut",
-        y: 0,
-        stagger: 0.1,
-        delay: delay2,
-      },
-      1
-    )
-    .fromTo(
-      Text1.chars,
-      { y: 0 },
-      {
-        y: -80,
-        duration: 1.2,
-        ease: "power3.inOut",
-        stagger: 0.1,
-        delay: delay,
-      },
-      0
-    )
-    .to(
-      Text2.chars,
-      {
-        y: -80,
-        duration: 1.2,
-        ease: "power3.inOut",
-        stagger: 0.1,
-        delay: delay2,
-      },
-      1
-    );
 }
