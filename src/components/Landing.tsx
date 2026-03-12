c03bb66fcb17b937fd80cd60a0acd6b6b1875c16
ChinmayaSingh <chinmaya.technology@gmail.com>
2026-03-12 12:44:23 +0530

    fix hero text overlap

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -6,6 +6,7 @@ const Landing = ({ children }: PropsWithChildren) => {
     <>
       <div className="landing-section" id="landingDiv">
         <div className="landing-container">
+
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
@@ -14,15 +15,21 @@ const Landing = ({ children }: PropsWithChildren) => {
               <span>SINGH</span>
             </h1>
           </div>
+
           <div className="landing-info">
             <h3>A SOFTWARE</h3>
+
             <h2 className="landing-info-h2">
               <div className="landing-h2-1">Developer</div>
-              <div className="landing-h2-2">Engineer</div>          
+              <div className="landing-h2-2">Engineer</div>
             </h2>
+
           </div>
+
         </div>
+
         {children}
+
       </div>
     </>
   );
