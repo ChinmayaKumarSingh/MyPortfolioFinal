726965c892b97ecd5b7771a4ee8e0d7f8118c31c
ChinmayaSingh <chinmaya.technology@gmail.com>
2026-03-12 18:49:46 +0530

    update

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -66,7 +66,7 @@ const Work = () => {
     <div className="work-section" id="work">
       <div className="work-container section-container">
         <h2>
-          Backend <span>Engineering</span> Projects
+          My <span>Engineering</span> Projects
         </h2>
 
         <div className="carousel-wrapper">
