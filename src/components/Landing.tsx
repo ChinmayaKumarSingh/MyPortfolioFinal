65be9e409a24624795b9d0d2c2908cec3f781253
ChinmayaSingh <chinmaya.technology@gmail.com>
2026-03-12 13:58:32 +0530

    fix landing hero section

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,41 +3,32 @@ import "./styles/Landing.css";
 
 const Landing = ({ children }: PropsWithChildren) => {
   return (
-    <div className="landing-section" id="landingDiv">
-
-      <div className="landing-container">
-
-        {/* LEFT SIDE */}
-
-        <div className="landing-intro">
-          <h2>Hello! I'm</h2>
-
-          <h1>
-            CHINMAYA
-            <br />
-            <span>SINGH</span>
-          </h1>
+    <>
+      <div className="landing-section" id="landingDiv">
+        <div className="landing-container">
+          <div className="landing-intro">
+            <h2>Hello! I'm</h2>
+            <h1>
+              CHINMAYA
+              <br />
+              <span>SINGH</span>
+            </h1>
+          </div>
+          <div className="landing-info">
+            <h3>A Software </h3>
+            <h2 className="landing-info-h2">
+              <div className="landing-h2-1">Developer</div>
+              <div className="landing-h2-2">Engineer</div>
+            </h2>
+            <h2>
+              <div className="landing-h2-info">Engineer</div>
+              <div className="landing-h2-info-1">Developer</div>
+            </h2>
+          </div>
         </div>
-
-        {/* RIGHT SIDE */}
-
-        <div className="landing-info">
-
-          <h3>A SOFTWARE</h3>
-
-          <h2 className="landing-info-h2">
-            DEVELOPER
-            <br />
-            ENGINEER
-          </h2>
-
-        </div>
-
+        {children}
       </div>
-
-      {children}
-
-    </div>
+    </>
   );
 };
 
